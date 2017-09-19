4e50e1b7aa8cbca90a2bd5abbd316de9cd434171
guo2205 <guolingfeng@192.168.1.16>
2017-09-19 10:12:55 +0800

    启动页和图标

---
+++ one by oneTests/one_by_oneTests.swift
@@ -0,0 +1,36 @@
+//
+//  one_by_oneTests.swift
+//  one by oneTests
+//
+//  Created by 郭凌峰 on 2017/9/18.
+//  Copyright © 2017年 郭凌峰. All rights reserved.
+//
+
+import XCTest
+@testable import one_by_one
+
+class one_by_oneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
